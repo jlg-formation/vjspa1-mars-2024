f9b727a6f13a9a711ac0ea4e9da8f03b4e04528c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-21 16:57:55 +0100

    dev: added list of articles

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -1,3 +1,44 @@
 <template>
-  <p>List works!</p>
+  <main>
+    <h1>Liste des articles</h1>
+    <div class="content">
+      <nav>
+        <button>Rafraîchir</button>
+        <RouterLink to="/stock/add" class="button">Ajouter</RouterLink>
+        <button>Supprimer</button>
+      </nav>
+      <div class="error"></div>
+      <table>
+        <thead>
+          <tr>
+            <th class="name">Nom</th>
+            <th class="price">Prix</th>
+            <th class="qty">Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="name">Tournevis</td>
+            <td class="price">2.56 €</td>
+            <td class="qty">123</td>
+          </tr>
+          <tr>
+            <td class="name">Pelle</td>
+            <td class="price">2.56 €</td>
+            <td class="qty">123</td>
+          </tr>
+          <tr>
+            <td class="name">Pioche</td>
+            <td class="price">2.56 €</td>
+            <td class="qty">123</td>
+          </tr>
+          <tr>
+            <td class="name">Marteau</td>
+            <td class="price">2.56 €</td>
+            <td class="qty">123</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
 </template>
