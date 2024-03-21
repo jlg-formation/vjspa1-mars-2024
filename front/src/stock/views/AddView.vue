318cafae437bf97c135743a5827593928b307630
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-21 17:05:12 +0100

    dev: add html

--- front/src/stock/views/AddView.vue
+++ front/src/stock/views/AddView.vue
@@ -1,3 +1,24 @@
 <template>
-  <p>Add works!</p>
+  <main>
+    <h1>Ajouter un article</h1>
+    <form>
+      <label>
+        <span>Nom</span>
+        <input type="text" />
+        <span class="error"></span>
+      </label>
+      <label>
+        <span>Prix</span>
+        <input type="number" />
+        <span class="error"></span>
+      </label>
+      <label>
+        <span>Quantité</span>
+        <input type="number" />
+        <span class="error"></span>
+      </label>
+      <div class="error"></div>
+      <button>Ajouter</button>
+    </form>
+  </main>
 </template>
