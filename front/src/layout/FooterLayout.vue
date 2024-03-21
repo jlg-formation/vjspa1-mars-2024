990b3b2b158b243def4046eb9b237d45b3e4c9b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-21 13:52:46 +0100

    dev: footer css ok

--- front/src/layout/FooterLayout.vue
+++ front/src/layout/FooterLayout.vue
@@ -1,3 +1,25 @@
 <template>
-  <p>Footer works !</p>
+  <footer>
+    <a href="#">Mentions Légales</a>
+  </footer>
 </template>
+
+<style scoped>
+footer {
+  background: #eee;
+  height: 3em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
