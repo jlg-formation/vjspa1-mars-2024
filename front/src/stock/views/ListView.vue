9fbc87512e869e4be413c429c48ff1d13f38d14d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-21 17:17:05 +0100

    dev: css

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -42,3 +42,47 @@
     </div>
   </main>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 0.25em;
+}
+
+div.error {
+  height: 2em;
+}
+
+table {
+  border: 0.1em solid #aaa;
+
+  border-collapse: separate;
+  border-spacing: 0;
+  border-radius: 0.3em;
+  overflow: hidden;
+
+  thead {
+    background: #aaa;
+  }
+
+  th,
+  td {
+    padding: 0.5em 1em;
+  }
+
+  tbody {
+    cursor: pointer;
+    tr:nth-child(even) {
+      background: #eee;
+    }
+
+    tr:hover {
+      background: #ddd;
+    }
+
+    tr.selected {
+      background: #ccc;
+    }
+  }
+}
+</style>
