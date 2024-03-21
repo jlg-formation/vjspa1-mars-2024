33057aee0e48a7d876acd9331650b3cafda4df31
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-21 12:15:49 +0100

    dev: css header

--- front/src/layout/HeaderLayout.vue
+++ front/src/layout/HeaderLayout.vue
@@ -1,3 +1,40 @@
 <template>
-  <p>Header works !</p>
+  <header>
+    <a href="#">
+      <img src="/logo.svg" alt="Logo" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
 </template>
+
+<style scoped>
+header {
+  background: #eee;
+  height: 3em;
+
+  display: flex;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    span {
+      font-weight: bold;
+      font-size: 1.3em;
+    }
+  }
+}
+
+img {
+  height: 2.5em;
+}
+</style>
