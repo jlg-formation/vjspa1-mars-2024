412831a93d75c9c11ec151912b540b8ea5b26521
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-22 14:12:48 +0100

    dev: added cors header

--- back/src/api.ts
+++ back/src/api.ts
@@ -9,6 +9,11 @@ const articles: Article[] = [
   { id: "a1", name: "Tournevis", price: 2.99, qty: 45 },
 ];
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/articles", (req, res) => {
   res.json(articles);
 });
