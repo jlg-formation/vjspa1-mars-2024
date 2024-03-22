f92f82b92df0b5bafa4e4e3a49baf4c20475d8f4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-22 16:10:59 +0100

    prod: no log

--- front/src/main.ts
+++ front/src/main.ts
@@ -6,6 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+if (import.meta.env.PROD) {
+  console.log = () => {}
+  console.error = () => {}
+  console.warn = () => {}
+  console.info = () => {}
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
